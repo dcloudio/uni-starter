0fef7c595f8687944403d865ba556103fde27913
anne-xml <liuxiaomei@dcloud.io>
2025-05-28 17:00:50 +0800

    Update settings.test.js

--- pages/ucenter/settings/settings.test.js
+++ pages/ucenter/settings/settings.test.js
@@ -46,9 +46,9 @@ describe('settings', () => {
 		if (!isHarmony) {
 			expect(texts).toContain('清理缓存');
 			expect(texts).toContain('推送功能');
-			unlockText = ios ? '人脸解锁' : '指纹解锁'
+			const unlockText = ios ? '人脸解锁' : '指纹解锁'
+			expect(texts).toContain(unlockText);
 		}
-		expect(texts).toContain(unlockText);
 		await page.callMethod('clearTmp')
 		const pushRes = await page.data('pushIsOn')
 		if (pushRes == "wait" && !isHarmony) {
