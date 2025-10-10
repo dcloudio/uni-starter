53fc5a497c9d8e00ec0852989bb16b3e5675e173
anne-xml <liuxiaomei@dcloud.io>
2025-10-10 20:38:42 +0800

    调整测试例顺序，避免web端登录页面截图被 ucenter 产生的 toast 污染

--- testSequencer.js
+++ testSequencer.js
@@ -1,5 +1,5 @@
 const Sequencer = require("@jest/test-sequencer").default;
-const sortTestFilenames = ["list.test.js","search.test.js","grid.test.js","register.test.js","login-withpwd.test.js","userinfo.test.js","ucenter.test.js","settings.test.js","about.test.js","pages.test.js"];
+const sortTestFilenames = ["list.test.js","search.test.js","grid.test.js","register.test.js","login-withpwd.test.js","userinfo.test.js","pages.test.js","ucenter.test.js","settings.test.js","about.test.js"];
 class CustomSequencer extends Sequencer {
   sort(tests) {
     // 测试例排序
